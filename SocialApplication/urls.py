cd0287993b9b0e5454b930aca1e04e6040bfa399
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-05-08 11:23:15 -0700

    Configured urlpatterns.

--- SocialApplication/urls.py
+++ SocialApplication/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('core.urls', 'core'), namespace='core')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
