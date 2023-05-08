f9f363400cf13f8bcfe9310868dc22f3c0a8ee89
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-05-08 13:28:45 -0700

    added Profile model.

--- core/models.py
+++ core/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Profile(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    phone = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile_images/', default='default.png')
+    location = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.user.username
