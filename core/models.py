9fcc3338ca9eea577158096cf6ff66ff5d287ad2
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-05-08 11:17:42 -0700

    Added profile model.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
