9aa60eead359f02ae91971b608b2a5ba25e2fb33
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-05-11 14:21:29 -0700

    Added Followers count to admin panel.

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 
 # Register your models here.
 admin_profile = admin.site.register(Profile)
 posts = admin.site.register(Post)
 likes = admin.site.register(LikePost)
+followers_count = admin.site.register(FollowersCount)
